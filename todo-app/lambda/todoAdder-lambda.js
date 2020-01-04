810a58ab84e70f148fe16c5bb1d93cf8d8fa742e
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-01-04 13:38:34 +0100

    Updated todo-adder lambda

--- todo-app/lambda/todoAdder-lambda.js
+++ todo-app/lambda/todoAdder-lambda.js
@@ -1,23 +1,42 @@
 const { DynamoDB } = require("aws-sdk");
 
 exports.handler = async function(event) {
-  console.log("request", JSON.stringify(event, null, 2));
-
-  // Create AWS SDK client:
   const dynamo = new DynamoDB();
+  const { queryStringParameters } = event;
+  let response = "OK";
+  let statusCode = 200;
+  console.log(queryStringParameters);
 
-  const params = {
-    "TableName": process.env.TODOS_TABLE_NAME,
-    "Item": {
-      todo: { S: "Learn DynamoDB" }
+  try {
+    if (queryStringParameters.todo) {
+      console.log("Putting a new item into DynamoDB");
+      await dynamo
+        .putItem({
+          "TableName": process.env.TODOS_TABLE_NAME,
+          "Item": {
+            todo: { S: queryStringParameters.todo }
+          }
+        })
+        .promise();
+    } else {
+      console.log("Scanning all items from the DB");
+      const scanResult = await dynamo
+        .scan({
+          "TableName": process.env.TODOS_TABLE_NAME,
+          "Limit": 10
+        })
+        .promise();
+      response = scanResult.Items;
     }
-  };
-
-  const result = await dynamo.putItem(params).promise();
+  } catch (error) {
+    console.log(error);
+    response = error;
+    statusCode = 500;
+  }
 
   return {
-    statusCode: 200,
+    statusCode,
     headers: { "Content-Type": "text/plain" },
-    body: JSON.stringify(result, null, 2)
+    body: response
   };
 };
