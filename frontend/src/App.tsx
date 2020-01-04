950b5731a45ff7c56c4e3555086d5e919b17ec61
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-01-04 17:07:43 +0100

    Added initial static version of the frontend app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 const App: React.FC = () => {
+  const inputElement = React.useRef<HTMLInputElement>(null);
+  const [todos, setTodos] = React.useState<Array<string>>([]);
+
+  React.useEffect(() => {
+    setTodos(["Do the thing", "Do the thing", "Do the thing"]);
+  }, []);
+
+  const renderTodos = () => todos.map(todo => <li>{todo}</li>);
+  const addTodo = (todo: string) => setTodos([...todos, todo]);
+
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    const value = inputElement?.current?.value;
+    if (event.key === "Enter" && value) {
+      addTodo(value);
+    }
+  };
+
+  const handleClick = () => {
+    const value = inputElement?.current?.value;
+    if (value) {
+      addTodo(value);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="cdk-todo">
+      <header className="cdk-todo--header">
+        <h1>Amazon CDK Todo App</h1>
       </header>
+      <label>
+        Add a new todo
+        <input type="text" ref={inputElement} onKeyPress={handleKeyPress} />
+        <button onClick={handleClick}>Add</button>
+      </label>
+      <div className="cdk-todo--list-container">
+        {todos.length > 0 ? (
+          <ul className="cdk-todo--list">{renderTodos()}</ul>
+        ) : (
+          <span>No todos added yet</span>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
