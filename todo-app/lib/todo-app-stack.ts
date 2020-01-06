fb91319811eb4dcbeca5032972ad700a2981ba0c
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-01-06 17:33:00 +0100

    Added logo asset and deployed an empty s3 bucket

--- todo-app/lib/todo-app-stack.ts
+++ todo-app/lib/todo-app-stack.ts
@@ -1,12 +1,15 @@
 import cdk = require("@aws-cdk/core");
 import apiGateway = require("@aws-cdk/aws-apigateway");
-import { TodoAdder } from "./todo-adder-stack";
+import s3 = require("@aws-cdk/aws-s3");
+import { TodoAdder } from "./todo-adder-construct";
 
 export class TodoAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const todoAdder = new TodoAdder(this, "TodoAdderStack", {});
+
+    new s3.Bucket(this, "LogoBucket", {});
 
     new apiGateway.LambdaRestApi(this, "TodoEndpoint", {
       handler: todoAdder.handler
