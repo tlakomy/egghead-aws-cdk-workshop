87d915cdef7600142b15cd72126b0f0f7f682294
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-02-09 17:18:50 +0100

    Added the S3 section

--- new-todo-app/lib/new-todo-app-stack.ts
+++ new-todo-app/lib/new-todo-app-stack.ts
@@ -2,14 +2,19 @@ import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as apiGateway from "@aws-cdk/aws-apigateway";
 import * as s3 from "@aws-cdk/aws-s3";
+import * as s3Notifications from "@aws-cdk/aws-s3-notifications";
+import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
 
 import { Duration } from "@aws-cdk/core";
+import { EventType } from "@aws-cdk/aws-s3";
 
 export class NewTodoAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const logoBucket = new s3.Bucket(this, "LogoBucket", {});
+    const logoBucket = new s3.Bucket(this, "LogoBucket", {
+      publicReadAccess: true
+    });
 
     const helloLambda = new lambda.Function(this, "HelloLambda", {
       runtime: lambda.Runtime.NODEJS_12_X,
@@ -22,6 +27,17 @@ export class NewTodoAppStack extends cdk.Stack {
 
     new apiGateway.LambdaRestApi(this, "Endpoint", {
       handler: helloLambda
+    });
+
+    logoBucket.addEventNotification(
+      EventType.OBJECT_CREATED,
+      new s3Notifications.LambdaDestination(helloLambda)
+    );
+
+    new s3Deployment.BucketDeployment(this, "DeployLogo", {
+      destinationBucket: logoBucket,
+      retainOnDelete: true, // keep current files
+      sources: [s3Deployment.Source.asset("./assets")]
     });
   }
 }
