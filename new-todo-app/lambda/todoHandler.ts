67ead14ec023ccf0699cce96ef1b9be987b20d3c
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-02-09 18:50:15 +0100

    Added handling POST and GET requests in the lambda handler

--- new-todo-app/lambda/todoHandler.ts
+++ new-todo-app/lambda/todoHandler.ts
@@ -1,20 +1,55 @@
 const { DynamoDB } = require("aws-sdk");
 
-exports.handler = async function(event: Record<string, any>) {
-  const dynamo = new DynamoDB();
-
-  const scanResult = await dynamo
-    .scan({
-      "TableName": process.env.TABLE_NAME,
-      "Limit": 10
-    })
-    .promise();
-
-  const response = scanResult.Items.map(DynamoDB.Converter.unmarshall);
-
+const createResponse = (body: string, statusCode = 200) => {
   return {
-    statusCode: 200,
+    statusCode,
     headers: { "Content-Type": "text/plain" },
-    body: JSON.stringify(response, null, 2)
+    body: JSON.stringify(body, null, 2)
   };
+};
+
+exports.handler = async function(event: AWSLambda.APIGatewayEvent) {
+  try {
+    const dynamo = new DynamoDB();
+    const { queryStringParameters, httpMethod } = event;
+    const tableName = process.env.TABLE_NAME;
+
+    if (httpMethod === "GET") {
+      const scanResult = await dynamo
+        .scan({
+          "TableName": tableName,
+          "Limit": 10
+        })
+        .promise();
+
+      const response = scanResult.Items.map(DynamoDB.Converter.unmarshall);
+
+      return createResponse(response);
+    }
+
+    if (httpMethod === "POST") {
+      if (queryStringParameters && queryStringParameters.todo) {
+        const { todo } = queryStringParameters;
+        await dynamo
+          .putItem({
+            "TableName": tableName,
+            "Item": {
+              id: { S: "random_id" },
+              todo: { S: todo }
+            }
+          })
+          .promise();
+
+        return createResponse(`${todo} added to the database`);
+      }
+      return createResponse("Todo is missing", 400);
+    }
+
+    return createResponse(
+      `We only accept GET, POST, and DELETE, not ${httpMethod}`,
+      400
+    );
+  } catch (error) {
+    return createResponse(error, 400);
+  }
 };
