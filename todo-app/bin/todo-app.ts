aa7e69957bd1e23aa0c36977ea7047190e068b16
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-02-09 19:16:19 +0100

    Removed the old todo-app

--- todo-app/bin/todo-app.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import cdk = require('@aws-cdk/core');
-import { TodoAppStack } from '../lib/todo-app-stack';
-
-const app = new cdk.App();
-new TodoAppStack(app, 'TodoAppStack');
