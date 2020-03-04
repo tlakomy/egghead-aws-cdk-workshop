b0d5d7d095bcf2834acd2648959aba9f0e580bad
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-03-04 22:42:06 +0100

    Added lesson 01

--- todo-app/bin/new-todo-app.ts
+++ todo-app/bin/new-todo-app.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { NewTodoAppStack } from '../lib/new-todo-app-stack';
+import * as cdk from "@aws-cdk/core";
+import { NewTodoAppStack } from "../lesson_01/lib/new-todo-app-stack"; // Change the stack number here at a beginning of each lesson
 
 const app = new cdk.App();
-new NewTodoAppStack(app, 'NewTodoAppStack');
+new NewTodoAppStack(app, "NewTodoAppStack");
--- todo-app/lesson_01/lambda/hello.ts
+++ todo-app/lesson_01/lambda/hello.ts
@@ -1,6 +1,5 @@
 exports.handler = async function(event: AWSLambda.APIGatewayEvent) {
     console.log("request:", JSON.stringify(event, null, 2));
-    console.log("Secret DB key", process.env.secret_db_key);
 
     return {
         statusCode: 200,
