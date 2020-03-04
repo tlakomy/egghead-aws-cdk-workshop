e2665414737963adf7656baecb4582cc78eaba9c
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-03-04 17:12:40 +0100

    Added cdk-spa-deploy

--- todo-app/lib/new-todo-app-stack.ts
+++ todo-app/lib/new-todo-app-stack.ts
@@ -2,43 +2,50 @@ import * as cdk from "@aws-cdk/core";
 import * as apiGateway from "@aws-cdk/aws-apigateway";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
+import { SPADeploy } from "cdk-spa-deploy";
 
 import { TodoDatabase } from "./todo-database";
 
 export class NewTodoAppStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const todoDatabase = new TodoDatabase(this, "TodoDatabase");
+        const todoDatabase = new TodoDatabase(this, "TodoDatabase");
 
-    const logoBucket = new s3.Bucket(this, "LogoBucket", {
-      publicReadAccess: true
-    });
+        const logoBucket = new s3.Bucket(this, "LogoBucket", {
+            publicReadAccess: true
+        });
 
-    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
-      publicReadAccess: true,
-      websiteIndexDocument: "index.html"
-    });
+        const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+            publicReadAccess: true,
+            websiteIndexDocument: "index.html"
+        });
 
-    new apiGateway.LambdaRestApi(this, "Endpoint", {
-      handler: todoDatabase.todoHandler
-    });
+        new apiGateway.LambdaRestApi(this, "Endpoint", {
+            handler: todoDatabase.todoHandler
+        });
 
-    new s3Deployment.BucketDeployment(this, "DeployLogo", {
-      destinationBucket: logoBucket,
-      retainOnDelete: true, // keep current files
-      sources: [s3Deployment.Source.asset("./assets")]
-    });
+        new s3Deployment.BucketDeployment(this, "DeployLogo", {
+            destinationBucket: logoBucket,
+            retainOnDelete: true, // keep current files
+            sources: [s3Deployment.Source.asset("./assets")]
+        });
 
-    new s3Deployment.BucketDeployment(this, "DeployWebsite", {
-      sources: [s3Deployment.Source.asset("../frontend/build")],
-      destinationBucket: websiteBucket
-    });
+        new SPADeploy(this, "spaDeploy").createSiteWithCloudfront({
+            indexDoc: "index.html",
+            websiteFolder: "../frontend/build"
+        });
 
-    new cdk.CfnOutput(this, "WebsiteUrl", {
-      value: websiteBucket.bucketWebsiteUrl
-    });
-  }
+        // OLD - deploying the site to an S3 bucket:
+        // new s3Deployment.BucketDeployment(this, "DeployWebsite", {
+        //     sources: [s3Deployment.Source.asset("../frontend/build")],
+        //     destinationBucket: websiteBucket
+        // });
+
+        // new cdk.CfnOutput(this, "WebsiteUrl", {
+        //     value: websiteBucket.bucketWebsiteUrl
+        // });
+    }
 }
 
 // Pre-database stack:
