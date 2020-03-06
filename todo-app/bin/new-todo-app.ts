928e944e6191d84df99170bc6456014141206e86
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-03-06 12:28:42 +0100

    Updated the todohandler final function

--- todo-app/bin/new-todo-app.ts
+++ todo-app/bin/new-todo-app.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from "@aws-cdk/core";
-import { NewTodoAppStack } from "../lesson_00/lib/new-todo-app-stack"; // Change the lesson number here at a beginning of each lesson
+import { NewTodoAppStack } from "../finished_state/lib/new-todo-app-stack"; // Change the lesson number here at a beginning of each lesson
 
 const app = new cdk.App();
 new NewTodoAppStack(app, "NewTodoAppStack");
