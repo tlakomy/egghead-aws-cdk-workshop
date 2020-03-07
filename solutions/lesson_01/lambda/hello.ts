12db6caab1cab457087912805b7aa68e7e0abe7e
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-03-07 19:36:29 +0100

    Removed the solutions directory since each lesson starts where the previous one has ended

--- solutions/lesson_01/lambda/hello.ts
+++
@@ -1,9 +0,0 @@
-exports.handler = async function(event: AWSLambda.APIGatewayEvent) {
-    console.log("request:", JSON.stringify(event, null, 2));
-
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "text/plain" },
-        body: `Hello, egghead friends! You've hit ${event.path}\n`
-    };
-};
