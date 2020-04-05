daba488b693d9fc907602d605bd33a402acbdde5
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-04-05 14:30:19 +0200

    Updated lesson 00

--- todo-app/lesson_00/lib/todo-app-stack.ts
+++ todo-app/lesson_00/lib/todo-app-stack.ts
@@ -6,5 +6,13 @@ import * as cdk from "@aws-cdk/core";
 export class TodoAppStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        const queue = new sqs.Queue(this, "TestCdkQueue", {
+            visibilityTimeout: cdk.Duration.seconds(300),
+        });
+
+        const topic = new sns.Topic(this, "TestCdkTopic");
+
+        topic.addSubscription(new subs.SqsSubscription(queue));
     }
 }
